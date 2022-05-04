7db434e2f4d1e92af6f2dffc485ddab59a5ba036
chinanwu <cawudev@gmail.com>
2022-05-03 19:31:48 -0700

    Akas for yakus

--- main.js
+++ main.js
@@ -10,12 +10,12 @@
 // 35 south
 // 36 west
 
+// This could be in a separate js file, but that means another file to pull
 const yakus = [
   {
     name: 'riichi',
     aka: 'ready hand',
     description: 'This is a special yaku applied to closed tenpai hands, which requires the player to make a declaration and "wager".',
-    // open: [false, 0], TODO: just have open be null if isn't possible?s
     closed: [true, 1],
     example: [ 1, 2, 3, 7, 7, 7, 25, 26, 27, 11, 12, 13, 36, 36]
   },
@@ -23,28 +23,25 @@ const yakus = [
     name: 'tsumo',
     aka: 'self draw',
     description: 'This yaku may apply to any closed hand. Any player who draws a winning tile with a closed hand is awarded this yaku.',
-    // open: [false, 0],
     closed: [true, 1],
     example: [ 1, 2, 3, 7, 7, 7, 25, 26, 27, 11, 12, 13, 36, 36]
   },
   {
     name: 'ippatsu',
-    aka: '',
+    aka: 'one shot',
     description: 'This is applied when the player calling riichi wins within 4 tile draws/discards. In addition, the cycle must not be interrupted by tile calls.',
-    // open: [false, 0],
     closed: [true, 1]
   },
   {
     name: 'pinfu',
-    aka: '',
+    aka: 'all sequences',
     description: 'Typically known as "all sequences", this is a hand that does not gain fu based on composition, other than that of a closed ron.',
-    // open: [false, 0],
     closed: [true, 1],
     example: [ 1, 2, 3, 5, 6, 7, 25, 26, 27, 11, 12, 13, 36, 36]
   },
   {
     name: 'iipeikou',
-    aka: '',
+    aka: 'identical sequences',
     description: 'This hand includes two identical sequences.',
     // open: [false, 0],
     closed: [true, 1],
@@ -52,35 +49,35 @@ const yakus = [
   },
   {
     name: 'haitei raoyue',
-    aka: '',
+    aka: 'win by last draw',
     description: 'This hand is completed with the last tile on the wall.',
     open: [true, 1],
     closed: [true, 1]
   },
   {
     name: 'houtei raoyui',
-    aka: '',
+    aka: 'win by last discard',
     description: 'This hand is completed with the very last discarded tile.',
     open: [true, 1],
     closed: [true, 1]
   },
   {
     name: 'rinshan kaihou',
-    aka: '',
+    aka: 'dead wall draw',
     description: 'This yaku is awarded upon winning with a tile from the dead wall.',
     open: [true, 1],
     closed: [true, 1]
   },
   {
     name: 'chankan',
-    aka: '',
+    aka: 'robbing a kan',
     description: 'The winning tile for a hand is called on another player\'s Kan. Essentially, the tile needed to complete a Kan is stolen to complete a winning hand.',
     open: [true, 1],
     closed: [true, 1]
   },
   {
     name: 'tanyao',
-    aka: '',
+    aka: 'all simples',
     description: 'A hand composed of only inside (numbers 2-8) tiles.',
     open: [true, 1],
     closed: [true, 1],
@@ -88,7 +85,7 @@ const yakus = [
   },
   {
     name: 'yakuhai',
-    aka: '',
+    aka: 'value tiles',
     description: 'A hand with at least one group of dragon tiles, seat wind, or round wind tiles. This hand can be valued at 1 han for each group.',
     open: [true, 1],
     closed: [true, 1],
@@ -96,14 +93,14 @@ const yakus = [
   },
   {
     name: 'double riichi',
-    aka: '',
+    aka: 'double ready',
     description: 'Riichi is declared with a dealt hand before the first discard.',
     // open: [false, 0],
     closed: [true, 1]
   },
   {
     name: 'chantaiyao',
-    aka: '',
+    aka: 'terminal or honor in each group',
     description: 'All tile groups contain at least 1 terminal or honor.',
     open: [true, 1],
     closed: [true, 2],
@@ -111,7 +108,7 @@ const yakus = [
   },
   {
     name: 'sanshoku doujun',
-    aka: '',
+    aka: 'three coloured straight',
     description: 'Three sequences have the same number across the three different suits.',
     open: [true, 1],
     closed: [true, 2],
@@ -119,7 +116,7 @@ const yakus = [
   },
   {
     name: 'ittsu',
-    aka: '',
+    aka: 'straight',
     description: 'This hand has a complete sequence of 1 through 9 of a single suit. As a note, the sequence of 1-9 are actually three groups of sequences numbered 123, 456, and 789.',
     open: [true, 1],
     closed: [true, 2],
@@ -127,7 +124,7 @@ const yakus = [
   },
   {
     name: 'toitoi',
-    aka: '',
+    aka: 'all triplets',
     description: 'The entire hand is composed of triplets.',
     open: [true, 2],
     closed: [true, 2],
@@ -135,7 +132,7 @@ const yakus = [
   },
   {
     name: 'sanankou',
-    aka: '',
+    aka: 'three concealed triplets',
     description: 'The hand includes three groups of triplets (or closed quads) that have been formed without calling any tiles. The fourth group can be an open triplet or sequence.',
     open: [true, 2],
     closed: [true, 2],
@@ -143,7 +140,7 @@ const yakus = [
   },
   {
     name: 'sanshoku douku',
-    aka: '',
+    aka: 'three coloured triplets',
     description: 'The hand includes three groups of triplets with the same number.',
     open: [true, 2],
     closed: [true, 2],
@@ -151,7 +148,7 @@ const yakus = [
   },
   {
     name: 'sankantsu',
-    aka: '',
+    aka: 'three kans',
     description: 'Three kans are called for this hand.',
     open: [true, 2],
     closed: [true, 2],
@@ -159,7 +156,7 @@ const yakus = [
   },
   {
     name: 'chiitoitsu',
-    aka: '',
+    aka: 'seven pairs',
     description: 'This hand is composed of seven pairs. It is one of two exceptions to the standard 4 tile groups and a pair pattern.',
     // open: [false, 0],
     closed: [true, 2],
@@ -167,7 +164,7 @@ const yakus = [
   },
   {
     name: 'honroutou',
-    aka: '',
+    aka: 'terminals and honours',
     description: 'The hand is composed of nothing but all terminals and honors. This hand may be considered as 4 han, because it is impossible to score this hand without either chii toitsu or toi toi.',
     open: [true, 2],
     closed: [true, 2],
@@ -175,7 +172,7 @@ const yakus = [
   },
   {
     name: 'shousangen',
-    aka: '',
+    aka: 'small three dragons',
     description: 'The hand contains two sets of 3 dragon tiles and a pair of the third dragon tiles. This hand may be considered as 4 han, because it is impossible to score this hand without two sets of yakuhai, from the two sets of dragons.',
     open: [true, 2],
     closed: [true, 2],
@@ -183,7 +180,7 @@ const yakus = [
   },
   {
     name: 'honitsu',
-    aka: '',
+    aka: 'half flush',
     description: 'This is a single suit hand mixed with some honor tiles.',
     open: [true, 2],
     closed: [true, 3],
@@ -191,7 +188,7 @@ const yakus = [
   },
   {
     name: 'junchan taiyao',
-    aka: '',
+    aka: 'terminal in each meld',
     description: 'All sets contain at least one terminal.',
     open: [true, 2],
     closed: [true, 3],
@@ -199,7 +196,7 @@ const yakus = [
   },
   {
     name: 'ryanpeikou',
-    aka: '',
+    aka: 'two sets of identical sequences',
     description: 'This hand has two sets of "iipeikou". This hand does not combine with chii toitsu, even though the hand can be interpreted as one.',
     // open: [false, 0],
     closed: [true, 3],
@@ -207,7 +204,7 @@ const yakus = [
   },
   {
     name: 'chinitsu',
-    aka: '',
+    aka: 'flush',
     description: 'This hand is composed entirely of tiles from only one of the three suits. It is the only yaku set at 6 han, where the number drops to 5 han when opened.',
     open: [true, 5],
     closed: [true, 6],
@@ -215,7 +212,7 @@ const yakus = [
   },
   {
     name: 'kazoe yakuman',
-    aka: '',
+    aka: 'counted yakuman',
     description: 'This is a special class of yakuman, where a hand is composed of 13+ han based on the combination of regular yaku and/or dora.',
     open: [true, 'yakuman'],
     closed: [true, 'yakuman'],
@@ -230,7 +227,7 @@ const yakus = [
   },
   {
     name: 'suuankou',
-    aka: '',
+    aka: 'four concealed triplets',
     description: 'This hand is composed of four groups of closed triplets. When this hand has a shanpon pattern and the win is via ron, then it would not be counted as such; only as the lesser toitoi with sanankou.',
     // open: [false, 0],
     closed: [true, 'yakuman'],
@@ -238,7 +235,7 @@ const yakus = [
   },
   {
     name: 'daisangen',
-    aka: '',
+    aka: 'big three dragons',
     description: 'This hand possesses three groups (triplets or quads) of all the dragons.',
     open: [true, 'yakuman'],
     closed: [true, 'yakuman'],
@@ -246,7 +243,7 @@ const yakus = [
   },
   {
     name: 'shousuushii',
-    aka: '',
+    aka: 'little four winds',
     description: 'This hand has three groups (triplets or quads) of the wind tiles plus a pair of the fourth kind.',
     open: [true, 'yakuman'],
     closed: [true, 'yakuman'],
@@ -254,7 +251,7 @@ const yakus = [
   },
   {
     name: 'daisuushii',
-    aka: '',
+    aka: 'big four winds',
     description: 'This hand has four groups (triplets or quads) of all four wind tiles.',
     open: [true, 'yakuman'],
     closed: [true, 'yakuman'],
@@ -262,7 +259,7 @@ const yakus = [
   },
   {
     name: 'tsuuiisou',
-    aka: '',
+    aka: 'all honours',
     description: 'Every group of tiles are composed of honor tiles.',
     open: [true, 'yakuman'],
     closed: [true, 'yakuman'],
@@ -270,7 +267,7 @@ const yakus = [
   },
   {
     name: 'chinroutou',
-    aka: '',
+    aka: 'all terminals',
     description: 'Every group of tiles are composed of terminal tiles.',
     open: [true, 'yakuman'],
     closed: [true, 'yakuman'],
@@ -278,7 +275,7 @@ const yakus = [
   },
   {
     name: 'ryuuiisou',
-    aka: '',
+    aka: 'all green',
     description: 'A hand composed entirely of green tiles: 2, 3, 4, 6 and 8 Sou and/or Hatsu.',
     open: [true, 'yakuman'],
     closed: [true, 'yakuman'],
@@ -286,15 +283,14 @@ const yakus = [
   },
   {
     name: 'chuuren poutou',
-    aka: '',
+    aka: 'nine gates',
     description: 'A hand consisting of the tiles 1112345678999 in the same suit plus any one extra tile of the same suit.',
-    // open: [false, 'yakuman'],
     closed: [true, 'yakuman'],
     example: [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9]
   },
   {
     name: 'suukantsu',
-    aka: '',
+    aka: 'four kans',
     description: 'Any hand with four calls of kan.',
     open: [true, 'yakuman'],
     closed: [true, 'yakuman'],
@@ -302,27 +298,28 @@ const yakus = [
   },
   {
     name: 'tenhou',
-    aka: '',
+    aka: 'heavenly hand',
     description: 'The dealer hand is a winning hand even before discarding a tile.',
-    // open: [false, 'yakuman'],
     closed: [true, 'yakuman'],
     example: [1, 2, 3, 6, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 22, 22]
   },
   {
     name: 'chiihou',
-    aka: '',
+    aka: 'earthly hand',
     description: 'The non-dealer hand is a winning hand with the first tile draw.',
-    // open: [false, 'yakuman'],
     closed: [true, 'yakuman'],
     example: [1, 2, 3, 6, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 22, 22]
   },
   {
     name: 'nagashi mangan',
-    aka: '',
+    aka: 'nagashi mangan',
     description: 'All the discards are terminals and/or honors. In addition, none of these discards were called by other players.',
-    // open: [false, 'yakuman'],
     closed: [true, 'yakuman'],
   },
 ];
 // TODO
 // - Consider how to make translation of yakus ?
+
+const getOpenHands = () => yakus.filter(({open}) => open);
+
+console.log(getOpenHands());
